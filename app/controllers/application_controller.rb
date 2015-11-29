ca2f8e5a4ba69d929e540f4ab2e0f2a13e4b420f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-11-29 18:08:11 -0500

    Set the min password length for devise invitable controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  before_filter :set_minimum_password_length, if: :devise_controller?
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:name, :password_confirmation]
